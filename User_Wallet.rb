72c4b264cb63c1650db48e96f9b9a15e64199d1f
Nelson Neuberger <nelson.neuberger@gmail.com>
2019-05-10 15:31:01 -0600

    Add wallet

--- User_Wallet.rb
+++ User_Wallet.rb
@@ -1,4 +1,25 @@
-# class User_Wallet
-#  puts "What is your name?"
+#require "pry"
 
-# end
+
+#class User_Wallet
+  attr_accessor :name, :wallet
+
+  def initialize
+    puts "What's your name?"
+    name = gets.chomp
+    sleep(1)
+    @name = name
+    @bankroll = 100.00
+    puts "What is your quest?"
+    sleep(1)
+    quest = gets.chomp
+    @quest = quest
+  end
+
+  def freshwallet
+    @wallet = 100.00
+  end
+end
+
+
+puts "hello"
