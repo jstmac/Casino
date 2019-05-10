97c0397c3bcd9d02bd636cf47c3913888a71a11c
Melissa G <jstmac@gmail.com>
2019-05-10 16:08:13 -0600

    Menu works

--- Roshambo.rb
+++ Roshambo.rb
@@ -2,16 +2,35 @@ require "pry"
 require "colorize"
 
 
-Puts "Welcome to the world renound Intense Game of Roshambo!"
-Puts "Otherwise known as Rock,Paper, Scissors"
-
-Puts "Get Ready, Set, Choose Your Poison!"
-
 class Roshambo
-    attr_accessor
+   
     def initialize
     @hand = [
       
     ]
+
+    puts "Welcome to the craziness of Roshambo!"
+
+    puts "Get Ready, Set, Choose Your Poison!"
+
+    puts "1) Rock"
+    puts "2) Paper"
+    puts "3) Scissors"
+    input = gets.to_i
+
+    puts "Nice choice...let's put it to the test"
+
+    sleep(3)
+
+    Puts "Dealer says..."
+    
+        
+
+    
+
+
+
   end 
 end
+
+roshambo = Roshambo.new
