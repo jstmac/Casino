36ccb02affdfbe0f8fc0f2c95f6d5bfe8cf6d8d8
Melissa <jstmac@gmail.com>
2019-05-10 15:23:53 -0600

    Test commit

---
+++ Roshambo.rb
@@ -0,0 +1,17 @@
+require "pry"
+require "colorize"
+
+
+Puts "Welcome to the world renound Intense Game of Roshambo!"
+Puts "Otherwise known as Rock,Paper, Scissors"
+
+Puts "Get Ready, Set, Choose Your Poison!"
+
+class Roshambo
+    attr_accessor
+    def initialize
+    @hand = [
+      
+    ]
+  end 
+end
